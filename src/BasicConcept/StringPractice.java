2b7ab285da98143dc452a8de1ba06d6d11eee3f0
jeethingarajia <hingarajiajeet@gmail.com>
2023-05-22 10:31:53 +0530

    project upload

---
+++ src/BasicConcept/StringPractice.java
@@ -0,0 +1,8 @@
+package BasicConcept;
+
+public class StringPractice {
+	public static void main(String[] args) {
+		System.out.println("i am in main method");
+		System.out.println("file A change");
+	}
+}
