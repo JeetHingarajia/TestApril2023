fe8165929c66df15430c6973e3397009602dde63
jeethingarajia <hingarajiajeet@gmail.com>
2023-05-22 13:30:29 +0530

    practice

--- src/A/File1.java
+++ src/A/File1.java
@@ -2,6 +2,9 @@ package A;
 
 public class File1 {
 	public static void main(String[] args) {
-		System.out.println("Branch A");  
+		System.out.println("Branch A"); 
+		System.out.println("jeet");
+		System.out.println("hingarajia");
+		System.out.println("april2023");
 	}
 }
