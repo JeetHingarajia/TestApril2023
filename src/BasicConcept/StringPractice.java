a062e987e1367a82997c3ac265da8387a4968eed
jeethingarajia <hingarajiajeet@gmail.com>
2023-05-23 11:45:55 +0530

    changes in pract.file

--- src/BasicConcept/StringPractice.java
+++ src/BasicConcept/StringPractice.java
@@ -2,7 +2,7 @@ package BasicConcept;
 
 public class StringPractice {
 	public static void main(String[] args) {
-		System.out.println("i am in main method");
-		System.out.println("file A change");
+		System.out.println("i am in main method.....of branch A");
+		System.out.println("creating conflit scenario from A brach");
 	}
 }
