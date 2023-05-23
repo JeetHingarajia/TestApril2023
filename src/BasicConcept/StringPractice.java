5400a4e4c6ea7341b1e3412caeb6d4662c736dfc
jeethingarajia <hingarajiajeet@gmail.com>
2023-05-23 12:18:42 +0530

    update in stringprac.file

--- src/BasicConcept/StringPractice.java
+++ src/BasicConcept/StringPractice.java
@@ -3,6 +3,6 @@ package BasicConcept;
 public class StringPractice {
 	public static void main(String[] args) {
 		System.out.println("i am in main method.....of branch A");
-		System.out.println("creating conflit scenario from A brach");
+		System.out.println("conflit resolved");
 	}
 }
